5d09a06edfc50ba637c9b7a37a0dab96e416da08
Kelan Mazey <kelanmazey@gmail.com>
2025-11-05 22:09:03 +0700

    add exit codes

--- app/main.go
+++ app/main.go
@@ -10,10 +10,15 @@ func main() {
 
 	running := true
 	for running {
-		fmt.Fprint(os.Stdout, "$ ")
+		fmt.Fprintln(os.Stdout, "$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
+		}
+
+		if command == "exit 0" {
+			fmt.Println(command)
+			running = false
 		}
 
 		fmt.Println(command[:len(command)-1] + ": command not found")
