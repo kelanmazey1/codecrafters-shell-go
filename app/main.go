7abc6c90adedc69cdc742b6f009e1a9cd0f2ab31
Kelan Mazey <kelanmazey@gmail.com>
2025-11-05 22:04:21 +0700

    add REPL loop

--- app/main.go
+++ app/main.go
@@ -7,11 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
+
+	running := true
+	for running {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
+
 	}
 
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
