466e5609adb457e8642fbcc2b7e296a95b9a41f7
Kelan Mazey <kelanmazey@gmail.com>
2025-11-05 21:54:42 +0700

    Fix test failure

--- app/main.go
+++ app/main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
-	command, err := bufio.NewReader(os.Stdin).ReadString("\n")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Println(command[:len(command)-1] + " : command not found")
 	fmt.Fprint(os.Stdout, "$ ")
